be86c5386e8e0ad89e001d97d56bd63d5d5ca52f
ccsds <40970041+ccsds@users.noreply.github.com>
2018-09-05 14:24:39 +0800

    Update jiedan.user.js

--- jiedan.user.js
+++ jiedan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         鹏博士工单系统自动接单
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       天堂小助手
 // @match        http://dzgd.drpeng.com.cn:8079/portal/r/w
@@ -49,8 +49,10 @@
         document.body.append(aud);
 
         document.getElementsByClassName("nav-area-img-panel")[0].click();
+        setTimeout(si,20000);
 
-        document.getElementById("logIndent").innerHTML="查询当前订单数\n"+document.getElementById("logIndent").innerHTML;
+                document.getElementById("logIndent").innerHTML="查询当前订单数\n"+document.getElementById("logIndent").innerHTML;
+
     },4000);
     function getIndent(len,nowTime){
 
@@ -79,27 +81,26 @@ case 1:
         break;
 }
         setTimeout(function(){
-            console.log("");
  document.getElementById("logIndent").innerHTML+="社区查询中...\n";
             var sheq='';
             for(var i =0 ;i<len;i++){
                 sheq=document.getElementsByClassName("metro-main-frame") [0].contentWindow.document.getElementsByClassName("aws-grid-row") [i].getElementsByClassName("pq-td-div")[3].innerHTML;
-                console.log();
+              
                  document.getElementById("logIndent").innerHTML=("社区是:"+sheq+"\n"+document.getElementById("logIndent").innerHTML);
-                 if(	sq.test(sheq)){
-
+                 if(!checked&&	sq.test(sheq)){
+                     document.getElementsByClassName("metro-main-frame") [0].contentWindow.document.getElementsByClassName("aws-grid-row")[0].getElementsByTagName ("input")[0].click();
                  document.getElementById("logIndent").innerHTML=("已选中社区："+sheq+"\n"+document.getElementById("logIndent").innerHTML);
                 checked=true;
-                document.getElementsByClassName("metro-main-frame") [0].contentWindow.document.getElementsByClassName("aws-grid-row")[0].getElementsByTagName ("input")[0].click();
-            }else if(len>1 && sq.test(sheq)){
+               
+            }else if(!checked&&len>1 && sq.test(sheq)){
                document.getElementById("logIndent").innerHTML=("已选中社区"+sheq+"\n"+document.getElementById("logIndent").innerHTML);
                 checked=true;
                 document.getElementsByClassName("metro-main-frame") [0].contentWindow.document.getElementsByClassName("aws-grid-row")[1].getElementsByTagName ("input")[0].click();
-            }else if(len>2 && sq.test(sheq)){
+            }else if(!checked&&len>2 && sq.test(sheq)){
                document.getElementById("logIndent").innerHTML=("已选中社区"+sheq+"\n"+document.getElementById("logIndent").innerHTML);
                 checked=true;
                 	document.getElementsByClassName("metro-main-frame") [0].contentWindow.document.getElementsByClassName("aws-grid-row")[2].getElementsByTagName ("input")[0].click();
-            }else if(len>3 && sq.test(sheq)){
+            }else if(!checked&&len>3 && sq.test(sheq)){
                document.getElementById("logIndent").innerHTML=("已选中社区"+sheq+"\n"+document.getElementById("logIndent").innerHTML);
                 checked=true;
                 document.getElementsByClassName("metro-main-frame") [0].contentWindow.document.getElementsByClassName("aws-grid-row")[3].getElementsByTagName ("input")[0].click();
@@ -109,11 +110,12 @@ case 1:
         },3000);
         setTimeout(function(){
             if(checked){
+                console.log("点击确定");
                 countIndent++;
                 document.getElementsByClassName("metro-main-frame") [0].contentWindow.document.getElementsByClassName("dlg-button")[0].getElementsByClassName ("blue")[0].click();
-            
+    
                  document.getElementById("logIndent").innerHTML=("接单成功，当前时间："+nowTime.toLocaleTimeString()+"\n"+document.getElementById("logIndent").innerHTML);
-               // document.getElementsByTagName("audio")[0].play();
+                document.getElementsByTagName("audio")[0].play();
 
             }else{
                  document.getElementById("logIndent").innerHTML="当前没有您的订单...\n"+document.getElementById("logIndent").innerHTML;
@@ -131,7 +133,7 @@ case 1:
       var nowTime=new Date();
 
         var len=document.getElementsByClassName("metro-main-frame")[0].contentWindow.document.getElementById("conTask").innerHTML*1;
-        console.log();
+       
           document.getElementById("logIndent").innerHTML=("当前订单数："+len+"\n"+document.getElementById("logIndent").innerHTML);
         setTimeout(function(){
             document.getElementsByClassName("metro-main-frame-refresh")[0].click();
