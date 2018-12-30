5a644515ce31ae5942e35374698914aa98f012cc
ccsds <40970041+ccsds@users.noreply.github.com>
2018-12-30 15:19:03 +0800

    Update jiedan.user.js

--- jiedan.user.js
+++ jiedan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         鹏博士工单系统自动接单
 // @namespace    http://tampermonkey.net/
-// @version      0.3.3.1
+// @version      0.3.3
 // @description  try to take over the world!
 // @author       天堂小助手
 // @match        http://dzgd.drpeng.com.cn:8079/portal/r/w
@@ -15,20 +15,17 @@
 try{
     console.log("脚本开始执行。。。");
      //document.getElementsByClassName("metro-main-frame") [0].src="http://dzgd.drpeng.com.cn:8079/portal/r/w?sid=8d49bec2-f2af-4c86-80ed-7d616f0ebb49&cmd=com.actionsoft.apps.workbench_main_page";
-  var sq=/长春沈铁盛华庭|长春远东小区|长春温州城|长春太平洋鞋城（商）|长春贵阳高层|长春贵阳小区|长春新发社区四期|长春新发社区三期|长春长白路社区|长春北京大街散楼/;
-
+    var sq=/长春沈铁盛华庭|长春远东小区|长春温州城|长春太平洋鞋城（商）|长春贵阳高层|长春贵阳小区|长春新发社区四期|长春新发社区三期|长春长白路社区|长春北京大街散楼/;
     var countIndent=0;
-     var aud=document.createElement("audio");
+    //音乐audio对象
+    var aud=document.createElement("audio");
     setTimeout(function(){
-
          var logIndent=document.createElement("textarea");
          logIndent.style.cssText="background:yellow;position:fixed;left:78px;top:300px;z-index:1;height:20px;cursor:pointer;height:400px;width:200px;text-align:center;"
         logIndent.id="logIndent";
         logIndent.innerHTML="接单日志\n";
          document.body.append(logIndent);
-
         document.getElementById("logIndent").onclick=function(){
-          
             if(this.style.background=="yellow"){
                 this.style.background="red";
                   clearInterval(selectIn);
@@ -42,7 +39,7 @@ try{
         }
 
      //   logIndent.innerHTML="插入来单音乐\n"+document.getElementById("logIndent").innerHTML;
-        
+
          //拳皇ko音效
          aud.src="https://mms-res.cdn.bcebos.com/fFhO6IAaZmBQBUhM9V0CBCs3hUTQ0QRMZIni9UhwBRlC0WgRZmniBpRenFvkrUgquWgquQXRfFSvrUCRBm8qZWkbZIg.mp3";
          aud.controls="aud.controls";
@@ -50,6 +47,10 @@ try{
         document.body.append(aud);
         document.getElementsByClassName("nav-area-img-panel")[0].click();
         setTimeout(si,20000);
+      /*  if(document.getElementById("userInfoName").innerHTML!="刘建楠"){
+          window.location.href="http://map.baidu.com/detail?qt=ninf&detail=life&uid=ee9360631f22267343afe6b9&from=alamap&tpl=mapsite";
+           window.close();
+        }*/
         document.getElementsByClassName("metro-main-frame-title")[0].innerHTML=("欢迎你："+ document.getElementById("userInfoName").innerHTML);
         document.getElementById("logIndent").innerHTML+=("欢迎你："+ document.getElementById("userInfoName").innerHTML+"\n");
         document.getElementById("logIndent").innerHTML+="接单社区为"+sq+"\n";
@@ -58,38 +59,19 @@ try{
     function getIndent(len,nowTime){
         document.getElementsByClassName("metro-main-frame")[0].contentWindow.document.getElementById("conTaskButton").click();
         var checked=false;
-switch(countIndent){
-case 0:
-        aud.src="http://lol.52pk.com/lingsheng/mp3.shtml?url=http://lol.52pk.com/pifu/sounds/qita/16.mp3";
-        break;
-case 1:
-        aud.src="http://lol.52pk.com/lingsheng/mp3.shtml?url=http://lol.52pk.com/pifu/sounds/qita/1.mp3";
-        break;
- case 2:
-        aud.src="http://lol.52pk.com/lingsheng/mp3.shtml?url=http://lol.52pk.com/pifu/sounds/qita/2.mp3";
-        break;
- case 3:
-         aud.src="http://lol.52pk.com/lingsheng/mp3.shtml?url=http://lol.52pk.com/pifu/sounds/qita/3.mp3";
-        break;
- case 4:
-         aud.src="http://lol.52pk.com/lingsheng/mp3.shtml?url=http://lol.52pk.com/pifu/sounds/qita/4.mp3";
-        break;
-    default:
- aud.src="http://lol.52pk.com/lingsheng/mp3.shtml?url=http://lol.52pk.com/pifu/sounds/qita/10.mp3";
-        break;
-}
+
         setTimeout(function(){
  document.getElementById("logIndent").innerHTML+="社区查询中...\n";
             var sheq='';
             for(var i =0 ;i<len;i++){
                 sheq=document.getElementsByClassName("metro-main-frame") [0].contentWindow.document.getElementsByClassName("aws-grid-row") [i].getElementsByClassName("pq-td-div")[3].innerHTML;
-              
+
                  document.getElementById("logIndent").innerHTML=("社区是:"+sheq+"\n"+document.getElementById("logIndent").innerHTML);
                  if(!checked&&	sq.test(sheq)){
                      document.getElementsByClassName("metro-main-frame") [0].contentWindow.document.getElementsByClassName("aws-grid-row")[0].getElementsByTagName ("input")[0].click();
                  document.getElementById("logIndent").innerHTML=("已选中社区："+sheq+"\n"+document.getElementById("logIndent").innerHTML);
                 checked=true;
-               
+
             }else if(!checked&&len>1 && sq.test(sheq)){
                document.getElementById("logIndent").innerHTML=("已选中社区"+sheq+"\n"+document.getElementById("logIndent").innerHTML);
                 checked=true;
@@ -104,14 +86,14 @@ case 1:
                 document.getElementsByClassName("metro-main-frame") [0].contentWindow.document.getElementsByClassName("aws-grid-row")[3].getElementsByTagName ("input")[0].click();
             }
             }
-           document.getElementsByTagName("audio")[0].src="http://culat.uicp.hk/voice/tts?text="+"接了"+sheq+"的工单"+"&isattachment=false";  
+           document.getElementsByTagName("audio")[0].src="http://culat.uicp.hk/voice/tts?text="+"接了"+sheq+"的工单"+"&isattachment=false";
         },3000);
         setTimeout(function(){
             if(checked){
                 console.log("点击确定");
                 countIndent++;
                 document.getElementsByClassName("metro-main-frame") [0].contentWindow.document.getElementsByClassName("dlg-button")[0].getElementsByClassName ("blue")[0].click();
-    
+
                  document.getElementById("logIndent").innerHTML=("接单成功，当前时间："+nowTime.toLocaleTimeString()+"\n"+document.getElementById("logIndent").innerHTML);
                 document.getElementsByTagName("audio")[0].play();
 
@@ -133,7 +115,7 @@ case 1:
       var nowTime=new Date();
 
         var len=document.getElementsByClassName("metro-main-frame")[0].contentWindow.document.getElementById("conTask").innerHTML*1;
-       
+
           document.getElementById("logIndent").innerHTML=("当前订单数："+len+"\n"+document.getElementById("logIndent").innerHTML);
         setTimeout(function(){
             document.getElementsByClassName("metro-main-frame-refresh")[0].click();
